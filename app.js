52f86204e1577da5a2ee4e420affd124f47e61b3
HiroPalacios <l2oot.control@gmail.com>
2020-10-30 21:57:24 -0500

    changed name

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!' + process.env.NODE_ENV + ' ' + process.env.AWS);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}, ${process.env.NODE_ENV}`)
 })
